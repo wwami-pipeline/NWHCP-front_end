d5abb73c245633602d85f94fe102d9b773a5d12c
natejfoster <nathan.96@comcast.net>
2018-02-28 16:16:37 -0800

    Created initial layout using css grid. Components will need to be built: cards, map, and header.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <div className="container">
+          <header className="appHeader">
+            <h1 className="appTitle">WWAMI Pipeline</h1>
+          </header>
+          <div className="seachBox">
+              <p>Seach Box</p>
+          </div>
+          <div className="resultsBox">
+            <p>results that are visible on the map shown here as cards</p>
+          </div>
+          <div className="mapBox">
+            <p>map goes here</p>
+          </div>
+        </div>
       </div>
     );
   }
