99dea8dd886f6c4c8a57179239fea16a63eddf6d
monicapharm <maliuqing123@gmail.com>
2019-05-18 00:14:15 -0700

    added page 2

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,89 @@
 import React, { Component } from 'react';
+import './about.css';
 
 class About extends React.Component {
     render() {
         return (
-            <p>hahaha</p>
+            <React.Fragment>
+                <div class="container">
+                    <section id="problem">
+                        <div class="row">
+                            <div class="column">
+                                <p>
+                                    <img class="left-image responsive-img" src="./assets/about.png" alt='about' />
+                                </p>
+                            </div>
+                            <div class="column">
+                                <p class="col m10 offset-m1 s12">
+                                    <h2 class="title center-align">How does the NWHCP site work?</h2>
+                                    <p class="flow-text">
+                                        Our map will link you to a variety of programs that will allow you to
+                                    </p>
+                                    <li class="flow-text">
+                                        Learn about oportunities in the <span class="highlight">health sciences and STEM</span>,
+                                    </li>
+                                    <li class="flow-text">
+                                        Experience <span class="highlight">hands-on activities</span>,
+                                    </li>
+                                    <li class="flow-text">
+                                        Prepare <span class="highlight">academically</span>,
+                                    </li>
+                                    <li class="flow-text">
+                                        Build your <span class="highlight">resume</span>,
+                                    </li>
+                                    <li class="flow-text">
+                                        Explore the <span class="highlight">wide variety of health based careers</span>. Learn about how to become a doctor, nurse, pharmacist, social worker, dentist, or therapist, and find out more about health professions that you might not even have heard of like health administration or health information technology!
+                                    </li>
+                                </p>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section id="programs">
+                        <div class="row">
+                            <h2 class="title center-align">
+                                What kind of programs are included?
+                            </h2>
+                            <p>
+                                Select from activities that will help you build your resume as you apply for school and work.
+                            </p>
+                        </div>
+
+                        <div>
+
+                            <ul class="features">
+                                <li>
+                                    <div class="feature-icon">
+                                        <div><img class="responsive-img " src="./assets/banner.png" alt='people' /></div>
+                                    </div>
+                                    <div class="feature-descr">
+                                        <p>Summer Camps</p>
+                                    </div>
+                                </li>
+                                <li>
+
+                                </li>
+                                <li>
+
+                                </li>
+                                <li>
+
+                                </li>
+                                <li>
+
+                                </li>
+                                <li>
+
+                                </li>
+                            </ul>
+
+
+                        </div>
+                    </section>
+                </div>
+
+
+            </React.Fragment>
         )
     }
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
                   <a className='buttonLink' href="/map">Check out our map</a>
                 </button>
                 <div class="center-align">
-                  <div><img class="responsive-img " src="./assets/people.png" alt='people' /></div>
+                  <div><img class="responsive-img " src="./assets/banner.png" alt='people' /></div>
                 </div>
               </div>
 
@@ -45,83 +45,8 @@ const Home = () => {
             </div>
           </div>
         </section>
-
-
-        <section id="problem">
-
-          <div class="row">
-            <div class="col m10 offset-m1 s12">
-              <h2 class="center-align">Why not Waypoint?</h2>
-              <p class="flow-text">
-                Since the 1970’s health professionals have known of a lack of primary care physicians
-                in the Greater Pacific Northwest region.
-                As the only accredited medical school in the area, the task fell on the UW School of Medicine
-                to solve this problem.
-                      </p>
-
-
-
-              <p class="flow-text">
-                Over the next decades the School of Medicine would create the
-                          <b>WWAMI initiative</b>, an innovative approach to promoting health care careers in underserved
-areas with targeted youth outreach and medical residencies.
-                      </p>
-
-              <h3>But there's a problem...</h3>
-
-              <p class="flow-text">
-                No one knows where this outreach is working and what areas need help. Students fall off the map, programs can't collaborate,
-                and the initiative suffers as a whole.
-                      </p>
-            </div>
-          </div>
-        </section>
-        <section id="solution">
-          <div class="row">
-            <div class="col m10 offset-m1 s12">
-              <h2 class="center-align">Guide the Way</h2>
-              <div class="center-align">
-                <img class="responsive-img" src="./assets/Map-Initial-View.svg" alt='map' />
-                <p class="italic-light">
-                  With the mapping tool, students can easily find relevant programs in their area.
-                          </p>
-              </div>
-
-
-              <p class="flow-text">
-                Waypoint offers students and partnering organizations a simple information hub for finding and connecting with outreach programs.
-                No longer do students risk the chance of not continuing their involment, and no longer do organizations
-                have to jump through hurdles just to connect and collaborate with similar offerings in their
-                area of operation.
-                      </p>
-              <h3>Process as a Product</h3>
-
-              <p class="flow-text">
-                The data that fuels Waypoint was created practically from scratch. We designed a method of data collection for the WWAMI
-                Inititiave that enables them to collect clean, extensive information for an ever growing number
-                of outreach programs. As more organizations provide this information, Waypoint will provide a
-                greater wealth of information.
-                      </p>
-              <div class="center-align">
-                <img class="responsive-img" src="./assets/steps.svg" alt='pipeline steps' />
-                <p class="italic-light">
-                  Creating a robust API for accessing the data collected allows future teams to utilize its potential to the fullest.
-                          </p>
-              </div>
-
-              <p class="flow-text">
-                Waypoint was built on the premise that good tools rely on good data. Knowing that organizational needs change, we made the
-                database that houses all of the information we've gathered available as its own tool. Through
-                our partner Seattle Data for Good, all of the details of this project and instructions for using
-                the data derived from it will be made available to an entire community of innovators who will
-                continue to help the WWAMI Initiative acheive its goal of eliminating the shortage of physicians
-                in areas of need.
-                      </p>
-            </div>
-          </div>
-        </section>
       </div>
-    </React.Fragment >
+    </React.Fragment>
   );
 };
 
