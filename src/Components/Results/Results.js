8b22f312c699e0c3e323329a2eb62fc834d3ab92
Anna Zhou <annaz4@uw.ed>
2021-05-08 10:50:13 -0700

    added fetch search results

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,11 +1,59 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import {Search} from './Search';
 // import {ResultCard} from './ResultCard';
 import {ResultGroup} from './ResultGroup';
 import {ResultMap} from './ResultMap';
 
 export class Results extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            programs: [],
+            error: null,
+            isLoaded: false,
+        }
+    }
+
+    componentDidMount() {
+        const sendData = {
+            "searchContent": "seattle",
+            "CareerEmp": ["Nursing"],
+            "HasCost": false,
+            "Under18":false, 
+            "HasTransport":false, 
+            "HasShadow":false, 
+            "GradeLevels":[]
+        }
+
+        const response = fetch("http://nwhealthcareerpath.uw.edu/api/v1/search", {
+            method: "POST",
+            body: JSON.stringify(sendData),
+            headers: new Headers({
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              })
+        })
+        .then(res => res.json())
+        .then(
+            (result) => {
+                console.log(result)
+                this.setState({
+                    isLoaded: true,
+                    programs: result
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        );
+    // console.log(this.state.programs);
+    }
+
     render() {
+        console.log(this.state.programs);
         return (
             <div>
                 {/* <Search /> */}
