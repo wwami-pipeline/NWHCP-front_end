3af07e0d55c91c3774e54ef8d4b0b5a44599e7b1
AJ <ancheetah@users.noreply.github.com>
2021-06-03 12:36:40 -0400

    removing node_modules from repo

--- node_modules/@react-leaflet/core/esm/pane.js
+++
@@ -1,8 +0,0 @@
-export function withPane(props, context) {
-  var _props$pane;
-
-  const pane = (_props$pane = props.pane) != null ? _props$pane : context.pane;
-  return pane ? { ...props,
-    pane
-  } : props;
-}
