3a15a66382fd433227275bbf82f8034ff95b412b
AJ <ancheetah@users.noreply.github.com>
2021-06-21 16:33:30 -0400

    Sass installation and setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'leaflet/dist/leaflet.css';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
