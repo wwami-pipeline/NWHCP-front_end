cd1d095eef8d93aba8a273484c1f0a850fb526f0
AJ <ancheetah@users.noreply.github.com>
2021-06-22 02:09:18 -0400

    style guide implementation pt 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
     Link
 } from 'react-router-dom';
 import {Navbar, Nav} from 'react-bootstrap';
-import {About} from './Components/About/About';
+import About from './Components/About/About';
 import {HowTo} from './Components/HowTo/HowTo';
 import {Footer} from './Components/Footer/Footer';
 import {Results} from './Components/Results/Results';
