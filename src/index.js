a8446d5b83723989370120bbf8e777b417e660fa
AJ <ancheetah@users.noreply.github.com>
2021-06-29 16:44:04 -0400

    moved bootstrap import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css'; // moved import to index.scss
 import 'leaflet/dist/leaflet.css';
 import './index.scss';
 import App from './App';
