65a5f088912b07bf98bcd3c65d8c4f054c2f8bc2
AJ <ancheetah@users.noreply.github.com>
2021-06-30 15:25:10 -0400

    converted component to functional with hooks

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {Navbar, Nav} from 'react-bootstrap';
 import About from './Components/About/About';
 import HowTo from './Components/HowTo/HowTo';
 import Footer from './Components/Footer/Footer';
-import {Results} from './Components/Results/Results';
+import Results from './Components/Results/Results';
 import {SignIn} from './Components/Account/SignIn';
 import SignUp from './Components/Account/SignUp';
 import {AccountSettings} from './Components/Account/AccountSettings';
--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,81 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Search from './Search';
 import ResultCard from './ResultCard';
-import {ResultMap} from './ResultMap';
+import { ResultMap } from './ResultMap';
 
-export class Results extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            programs: [],
-            error: null,
-            isLoaded: false,
-            dataToSend: {
-                "searchContent": "seattle",
-                "CareerEmp": ["Nursing"],
-                "HasCost": false,
-                "Under18":false, 
-                "HasTransport":false, 
-                "HasShadow":false, 
-                "GradeLevels":[]
-            }
-        }
-    }
-
-    componentDidMount() {
-        const sendData = {
-            "searchContent": "seattle",
-            "CareerEmp": ["Nursing"],
-            "HasCost": false,
-            "Under18":false, 
-            "HasTransport":false, 
-            "HasShadow":false, 
-            "GradeLevels":[]
-        }
-
-        const response = fetch("http://nwhealthcareerpath.uw.edu/api/v1/search", {
-            method: "POST",
-            body: JSON.stringify(sendData),
-            headers: new Headers({
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              })
-        })
-        .then(res => res.json())
-        .then(
-            (result) => {
-                console.log(result)
-                this.setState({
-                    isLoaded: true,
-                    programs: result
-                });
-            },
-            (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                });
-            }
-        );
-    // console.log(this.state.programs);
-    }
-
-    render() {
-
-        const programCards = this.state.programs.map( (program, index) => {
-            return (
-                <div key={index}>
-                    <ResultCard program={program} />
-                </div>
-            )
-        })
-
+const RenderPrograms = (props) => {
+    return props.programs.map((program, index) => {
         return (
-            <div>
-                <Search />
-                <ResultMap programs={this.state.programs}/>
-                <div className="mt-5">{programCards}</div>
+            <div key={index}>
+                <ResultCard program={program} />
             </div>
-        )
-    }
+        );
+    });
 }
+
+const Results = () => {
+
+    const [formData, setFormData] = useState({
+                                                searchContent: 'seattle',
+                                                CareerEmp: ['Nursing'],
+                                                HasCost: false,
+                                                Under18: false,
+                                                HasTransport: false,
+                                                HasShadow: false,
+                                                GradeLevels: [] 
+                                            });
+    const [programs, setPrograms] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect( () => {
+        setLoading(true);
+        console.log("fetching...");
+
+        fetch('http://nwhealthcareerpath.uw.edu/api/v1/search', 
+            {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: new Headers({
+                                        'Accept': 'application/json',
+                                        'Content-Type': 'application/json'
+                                    })
+            }
+        )
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    // console.log("fetched data: ", result);
+                    setPrograms(result);
+                },
+                (error) => {
+                    setLoading(false);
+                    setError(error);
+                }
+            )
+            .finally(setLoading(false));
+    }, []);
+
+    if (loading) {
+        return (
+            <p>Loading Programs...</p>
+        );
+    }
+
+    return (
+        <div>
+            <Search />
+            <ResultMap programs={programs} />
+            <div className='mt-5'>
+                <RenderPrograms programs={programs} />
+            </div>
+        </div>
+    );
+};
+
+export default Results;
