4ff33e0b611cf5e0372c1556f9e9f62da6e3d38b
Loi Cheng <loi@loico.us>
2021-09-04 23:22:20 -0400

    certbot

--- build-prod.sh
+++ build-prod.sh
@@ -1,2 +1,4 @@
 # run on webserver
 docker run -d --network host --rm --pull=always --name nwhcpfrontend ghcr.io/wwami-pipeline/nwhcp-front_end:development
+
+docker run --network host --rm --pull=always --name nwhcpfrontend ghcr.io/wwami-pipeline/nwhcp-front_end:development-nightly
