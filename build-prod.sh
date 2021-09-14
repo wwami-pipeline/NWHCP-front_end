6462d002425786b6a2e07da79f79dad9529502e9
Loi Cheng <loi.cheng@gmail.com>
2021-09-13 22:39:03 -0400

    update deploy code

--- build-prod.sh
+++ build-prod.sh
@@ -1,4 +1,2 @@
 # run on webserver
 docker run -d --rm --network host --pull=always -v /etc/letsencrypt:/etc/letsencrypt --name nwhcpfrontend ghcr.io/wwami-pipeline/nwhcp-front_end:development
-
-docker run -d --rm --network host --pull=always -v /etc/letsencrypt:/etc/letsencrypt --name nwhcpfrontend ghcr.io/wwami-pipeline/nwhcp-front_end:development-nightly
--- build-pull.sh
+++
@@ -1,2 +0,0 @@
-# Run server from docker img
-docker run --rm --network host --name nwhcpfrontend ghcr.io/wwami-pipeline/nwhcp-front_end:development;
