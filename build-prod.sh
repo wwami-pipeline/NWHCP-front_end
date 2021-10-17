3b3bbd8e022282f09350e6893c90e1352598553a
Loi Cheng <loi@preverna.com>
2021-10-16 22:44:22 -0400

    Update build-prod.sh

--- build-prod.sh
+++ build-prod.sh
@@ -1,2 +1,2 @@
-# run on webserver
-docker run -d --rm --network host --pull=always -v /etc/letsencrypt:/etc/letsencrypt --name nwhcpfrontend ghcr.io/wwami-pipeline/nwhcp-front_end:development
+# run on webserver manually to udpate 
+docker run -d --rm --network host --pull=always -v /etc/letsencrypt:/etc/letsencrypt --name nwhcpfrontend ghcr.io/wwami-pipeline/nwhcp-front_end:main
