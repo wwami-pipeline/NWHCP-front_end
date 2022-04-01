5525dcf9328682ca87db90dd3224557e2c92c9ed
hoang nguyen <hoangng@cs.washington.edu>
2022-03-31 20:42:43 -0700

    fix: remove unused

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../scss/footers.scss';
 import { Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhoneAlt, faEnvelope, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 // test
 
