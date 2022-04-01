fe2361c42eab0754a186eb89eba62799e76dfc1a
hoang nguyen <hoangng@cs.washington.edu>
2022-03-31 20:17:57 -0700

    Revert "feat: basic footer setup"
    This reverts commit dbc8d3cb5b45caa55b6d43422679e96f65ef0171.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,61 +1,35 @@
 import React from 'react';
 import '../scss/footers.scss';
-import { Container, Row, Col } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhoneAlt, faEnvelope, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import {Container, Row, Col} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPhoneAlt, faEnvelope} from '@fortawesome/free-solid-svg-icons';
 
 // test
 
 const Footer = () => {
-  return (
-    <Container fluid className="footer main-footer py-3 mt-4" style={{ padding: "40px 15%" }}>
-      <h2 style={{ color: "white" }}>
-        Contact
-      </h2>
-      <Row>
-        <Col xs={5}>
-          <Row>
-            <Col xs={3}>
-              <FontAwesomeIcon icon={faUserCircle} style={{ height: 64, width: 64 }} />
-            </Col>
-            <Col xs={9}>
-              <Row>
-                <h4 className='footer'>Name, Position</h4>
-                <h4 className='sub-text-footer'>Department</h4>
-              </Row>
-              <Row style={{ marginTop: 24 }}>
-                <h4 className='sub-text-footer'>email@gmail.com</h4>
-                <h4 className='sub-text-footer'>(206)221-4613</h4>
-                <h4 className='sub-text-footer'>4200 Whitman Ct NE, Seattle, WA</h4>
-              </Row>
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={5}>
-          <Row>
-            <Col xs={3}>
-              <FontAwesomeIcon icon={faEnvelope} style={{ height: 64, width: 64 }} />
-            </Col>
-            <Col xs={9}>
-              <Row>
-                <h4 className='footer'>Mailing address</h4>
-              </Row>
-              <Row style={{ marginTop: 24 }}>
-                <h4 className='sub-text-footer'>4200 Whitman Ct NE,</h4>
-                <h4 className='sub-text-footer'>Seattle, 98195</h4>
-                <h4 className='sub-text-footer'>WA</h4>
-              </Row>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12}>
-          <p className="small mt-2">NWHCP ©2021</p>
-        </Col>
-      </Row>
-    </Container>
-  );
+    return (
+        <Container fluid className="footer main-footer py-3 mt-4">
+            <Row>
+                <Col xs={6}>
+                    <p>
+                        <FontAwesomeIcon icon={faPhoneAlt} className="mr-2"/>
+                        (206)221-4613
+                    </p>
+                </Col>
+                <Col xs={6}>
+                    <p>
+                        <FontAwesomeIcon icon={faEnvelope} className="mr-2"/>
+                        somserve@uw.edu
+                    </p>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    <p className="small mt-2">NWHCP ©2021</p>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default Footer;
