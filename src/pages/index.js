b3dddfbf85023c3f6b613c0d2eb901367a904581
Tyrah <tyrah96@gmail.com>
2023-02-05 21:28:42 -0800

    more styles added and positioning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 /* main page */
 import React from "react";
 import { Link } from "gatsby";
-import { Button, Carousel } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import Students from "../images/nwhcp-students.png";
 
 const Home = () => {
   return (
@@ -20,7 +21,7 @@ const Home = () => {
             textAlign: "center",
           }}
         >
-          There's something for every student in healthcare
+          There's something for every student in healthcare.
         </h1>
       </div>
       <div
@@ -28,19 +29,20 @@ const Home = () => {
         style={{
           textAlign: "left",
           flex: "1",
-          margin: "50px",
           display: "flex",
           flexDirection: "column",
           alignContent: "flex-end",
         }}
       >
-        {/* <img src={require("/images/home/doctors.svg")} atl="" /> */}
         <p
           style={{
             fontSize: "1.2rem",
             lineHeight: "2rem",
-            width: "75%",
+            width: "60%",
             fontSize: "25px",
+            position: "relative",
+            left: "655px",
+            top: "200px",
           }}
         >
           Made for students pursuing a career in the health professions.
@@ -50,25 +52,49 @@ const Home = () => {
         <p
           className="lead "
           style={{
-            width: "75%",
+            width: "60%",
             fontSize: "25px",
+            position: "relative",
+            left: "655px",
+            top: "200px",
           }}
         >
           Let's match you up with programs that might be right for you!
         </p>
+        <img
+          src={Students}
+          atl="students"
+          style={{
+            width: "700px",
+            height: "700px",
+            position: "relative",
+            bottom: "290px",
+            right: "70px",
+          }}
+        />
       </div>
-      <div className="text-center pb-4">
+      <div
+        className="text-center pb-4"
+        style={{
+          position: "relative",
+          bottom: "300px",
+        }}
+      >
         <Link to="/programs">
           <Button
             size="lg"
             variant="primary"
             style={{
               borderRadius: "10px",
-              padding: "20px",
+              border: "1px solid #101010",
+              boxShadow: "2px 5px 2px 1px #949494",
+              padding: "29px",
+              fontSize: "26px",
               fontWeight: "bold",
+              width: "20%",
             }}
           >
-            Get Started!
+            Get Started
           </Button>
         </Link>
       </div>
@@ -76,10 +102,9 @@ const Home = () => {
         className=""
         style={{
           position: "relative",
-          bottom: "-3rem",
+          bottom: "280px",
           textAlign: "center",
-          fontSize: "20",
-          flex: "1",
+          fontSize: "40",
         }}
       >
         Click <Link to="/about">here</Link> to learn more about us.
