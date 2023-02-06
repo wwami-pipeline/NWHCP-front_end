e8247a9d4fd161ea9ba16fcbdc9ee583ec0827e5
Tyrah <tyrah96@gmail.com>
2023-02-05 18:56:21 -0800

    updating homepage flexbox and grid positions

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,49 +11,79 @@ const Home = () => {
         height: "100vh",
         display: "flex",
         flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
       }}
     >
-      <div className="page-wrapper text-center">
-        <h2 className="text-primary" style={{ fontSize: "1.3rem" }}>
-          There's something for every student in healthcare
-        </h2>
-        <div
-          className="d-flex"
-          style={{ alignItems: "center", textAlign: "left" }}
+      <div>
+        <h1
+          className="text-primary"
+          style={{
+            textAlign: "center",
+          }}
         >
-          {/* <img src={require("/images/home/doctors.svg")} atl="" /> */}
-          <div>
-            <p style={{ fontSize: "1.2rem", lineHeight: "2rem" }}>
-              Made for students pursuing a career in the health professions.
-              <br /> Explore our site to find supplemental programs and
-              opportunities that will help you reach your goals.
-            </p>
-            <p className="lead ">
-              Let's match you up with programs that might be right for you!
-            </p>
-          </div>
-        </div>
-        <div className="text-center pb-4">
-          <Link to="/programs">
-            <Button
-              size="lg"
-              variant="primary"
-              style={{
-                borderRadius: "10px",
-                padding: "20px",
-                fontWeight: "bold",
-              }}
-            >
-              Get Started!
-            </Button>
-          </Link>
-        </div>
-        <p className="" style={{ position: "relative", bottom: "-3rem" }}>
-          Click <Link to="/about">here</Link> to learn more about us.
+          There's something for every student in healthcare
+        </h1>
+      </div>
+      <div
+        className="d-flex"
+        style={{
+          textAlign: "left",
+          flex: "1",
+          margin: "50px",
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "flex-end",
+        }}
+      >
+        {/* <img src={require("/images/home/doctors.svg")} atl="" /> */}
+        <p
+          style={{
+            fontSize: "1.2rem",
+            lineHeight: "2rem",
+            width: "75%",
+            fontSize: "25px",
+          }}
+        >
+          Made for students pursuing a career in the health professions.
+          <br /> Explore our site to find supplemental programs and
+          opportunities that will help you reach your goals.
+        </p>
+        <p
+          className="lead "
+          style={{
+            width: "75%",
+            fontSize: "25px",
+          }}
+        >
+          Let's match you up with programs that might be right for you!
         </p>
       </div>
+      <div className="text-center pb-4">
+        <Link to="/programs">
+          <Button
+            size="lg"
+            variant="primary"
+            style={{
+              borderRadius: "10px",
+              padding: "20px",
+              fontWeight: "bold",
+            }}
+          >
+            Get Started!
+          </Button>
+        </Link>
+      </div>
+      <p
+        className=""
+        style={{
+          position: "relative",
+          bottom: "-3rem",
+          textAlign: "center",
+          fontSize: "20",
+          flex: "1",
+        }}
+      >
+        Click <Link to="/about">here</Link> to learn more about us.
+      </p>
     </div>
   );
 };
