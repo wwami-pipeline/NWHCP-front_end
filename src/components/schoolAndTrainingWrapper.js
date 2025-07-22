e32bd84a965f5ba4ea5fa3f695f581d9b50a48bc
Nick Edwards <nickedwards.dev@gmail.com>
2025-07-22 11:23:35 -0700

    Fitler updates and styling for About page

--- src/components/schoolAndTrainingWrapper.js
+++ src/components/schoolAndTrainingWrapper.js
@@ -2,21 +2,20 @@ import { Grid } from "@mui/material";
 import React from "react";
 import PathwayDropdown from "./PathwayDropdown";
 
-
 export default function SchoolAndTrainingWrapper() {
   return (
     <>
       {/* Path */}
-      <PathwayDropdown level={0} title={"More"}>
+      <PathwayDropdown level={0} title={"Examples"}>
         <Grid>
           <li>Academic Health Professions Schools</li>
           <li>Health Professions Training Program Sites (for credit)</li>
           <li>Competency-based Training Program</li>
           <li>Career Technical Education Centers</li>
-          <li>Inter-Professional Education Training Centers</li>
+          <li>Interprofessional Education Training Centers</li>
           <li>Apprenticeship Programs</li>
         </Grid>
       </PathwayDropdown>
     </>
-  )
+  );
 }
